cb66636e1a59683ff28c389500ad52945e527a19
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2026-02-24 00:27:47 +0200

    functie de comparare, dezalocare pe structura Product

--- ActivitateStructuriDateDumitrescu_Alexandra_2026/Seminar1-Practice1.c
+++ ActivitateStructuriDateDumitrescu_Alexandra_2026/Seminar1-Practice1.c
@@ -63,6 +63,13 @@ void afisare(struct Product p) {
 //
 //Crucial : Frees the old memory before allocating the new memory to prevent leaks.
 //
+void updateBrandName(struct Product* product,char* brand) {
+	if (strlen(brand) > 2) {
+		free(product->brand);
+		product->brand = (char*)malloc(sizeof(char) * strlen(brand) + 1);
+		strcpy(product->brand, brand);
+	}
+}
 //5. Conditional Logic(Comparison)
 //Write a function getCheaperProduct that :
 //
@@ -71,18 +78,38 @@ void afisare(struct Product p) {
 //Returns the brand name of the product with the lower price.
 //
 //Edge case: If prices are equal, return the name of the first one.
-//
+char* getCheaperProduct(struct Product p1, struct Product p2) {
+	if (p1.price <= p2.price) {
+		return p1.brand;
+	}
+	return p2.brand;
+}
 //	6. Memory Cleanup
 //	Write a function destroyProduct that :
 //
 //Takes a pointer to a product.
 //
 //Frees the brand memory and sets the pointer to NULL to avoid "dangling pointers."
+void dezalocare(struct Product* product) {
+	free(product->brand);
+	product->brand = NULL;
+}
 
 
 int main() {
 	struct Product p;
 	p = initializare(100, "Samsung", 999.99, 46, 'A');
 	afisare(p);
+	updateBrandName(&p, "Apple");
+	afisare(p);
+	struct Product p1;
+	struct Product p2;
+	p1 = initializare(100, "Produs 1 marca", 999.99, 46, 'A');
+	p2 = initializare(100, "Produs 2 marca", 999.99, 46, 'A');
+	printf("%s", getCheaperProduct(p1,p2));
+	dezalocare(&p);
+	dezalocare(&p1);
+	dezalocare(&p2);
+
 	return 0;
 }
